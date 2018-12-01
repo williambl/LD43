cafa64fc81992b7a3a701925bb26763d20334168
williambl <william@williambl.com>
2018-12-01 13:42:40 +0000

    Add resource class

---
+++ LD43/Assets/Scripts/Resource.cs
@@ -0,0 +1,11 @@
+﻿public class Resource {
+
+    string name;
+
+    int amount;
+    
+    public Resource(string name, int amount) {
+        this.name = name;
+        this.amount = amount;
+    }
+}
