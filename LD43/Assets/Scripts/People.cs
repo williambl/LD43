6c235b1ddd26954e9b57783b2df7e4a863b15791
williambl <william@williambl.com>
2018-12-01 13:56:12 +0000

    Add happiness to People

--- LD43/Assets/Scripts/People.cs
+++ LD43/Assets/Scripts/People.cs
@@ -3,7 +3,15 @@ public class People {
     public float happiness;
     public int amountOfPeople;
 
+    float happinessPerSecond;
+
     public People() {
-    
+        happiness = 0.5;
+        amountOfPeople = 500;
+        happinessPerSecond = -0.01;
+    }
+
+    public void Update() {
+        happiness += happinessPerSecond;
     }
 }
