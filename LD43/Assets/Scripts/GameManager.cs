65d840e20fd5d65a8588219ea196b684e38117c6
williambl <william@williambl.com>
2018-12-01 14:06:19 +0000

    Add Coroutine to update everything every second

--- LD43/Assets/Scripts/GameManager.cs
+++ LD43/Assets/Scripts/GameManager.cs
@@ -14,6 +14,17 @@ public class GameManager : MonoBehaviour {
         resources = new Resources();
         people = new People();
         enemies = new Enemies();
+        StartCoroutine(UpdateAllThings);
+    }
+
+    IEnumerator UpdateAllThings () {
+        while (true) {
+            yield return new WaitForSeconds(1.0f);
+            resources.Update();
+            People.Update();
+            Gods.Update();
+            Enemies.Update();
+        }
     }
 
 }
