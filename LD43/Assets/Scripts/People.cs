cc1abebfe8e4c05676518d446e01b64d77222061
williambl <william@williambl.com>
2018-12-01 20:20:31 +0000

    Add population growth

--- LD43/Assets/Scripts/People.cs
+++ LD43/Assets/Scripts/People.cs
@@ -1,17 +1,27 @@
+using UnityEngine;
+
 public class People {
 
     public float happiness;
     public int amountOfPeople;
 
     float happinessPerSecond;
+    int secondsPerPeople;
 
     public People() {
         happiness = 0.5f;
         amountOfPeople = 500;
         happinessPerSecond = -0.01f;
+        secondsPerPeople = 5;
     }
 
     public void Update() {
         happiness += happinessPerSecond;
+
+        if (((int)Time.time % secondsPerPeople) == 0)
+            amountOfPeople++;
+
+        Debug.Log("Happiness: " + happiness);
+        Debug.Log("People: " + amountOfPeople);
     }
 }
